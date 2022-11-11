b3f36a8017a0b16ea323816409c90eeb1d364977
Dante Allccahuaman <dantemon19@gmail.com>
2022-11-11 15:57:46 -0500

    descuento2

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const montoDesc = document.querySelector('#monto');
 const descuentoDesc = document.querySelector('#descuento');
 const calcular = document.querySelector('#calcular');
 const modificarCal = document.querySelector('#modificar');
+const modificarCal2 = document.querySelector('#modificar2');
 
 calcular.addEventListener('click', calcularDes);
 
@@ -11,7 +12,8 @@ function calcularDes (){
     if(descuentoDesc.value < 100){
         let descuento = montoDesc.value * (descuentoDesc.value / 100);
         let final = montoDesc.value - descuento;
-        modificarCal.innerHTML = final + "<br>" + "El descuento es " + descuento;
+        modificarCal.innerHTML = final;
+        modificarCal2.innerHTML = descuento;
     }else{
         modificarCal.innerHTML = "Ingresar descuento entre 1% y 100%"
     }
